dc922e3540853a23aec6715dcab840ad32fc1c79
Matthew Rudy Jacobs <MatthewRudyJacobs@gmail.com>
2010-08-03 23:31:09 +0100

    refactor the combos

--- case.rb
+++ case.rb
@@ -1,8 +1,51 @@
 class Case
-  attr_accessor :grid
   
-  def score
-    Scorer.score(self.grid)
+  COMBOS = [
+    [ 0, 1, 2, 3], # row 1
+    [ 4, 5, 6, 7], # row 2
+    [ 8, 9,10,11], # row 3
+    [12,13,14,15], # row 4
+
+    [ 0, 4, 8,12], # col 1
+    [ 1, 5, 9,13], # col 2
+    [ 2, 6,10,14], # col 3
+    [ 3, 7,11,15], # col 4
+  ]
+  
+  def all_combos
+    COMBOS.inject([]) do |combos, indices|
+      all_4 = grid.values_at(*indices)
+      combos << all_4
+      combos << all_4[0,3]
+      combos << all_4[1,3]
+
+      combos
+    end
   end
+  
+  def all_words
+    all_combos.select do |combo|
+      is_word?(combo)
+    end
+  end
+  
+  def initialize(grid)
+    @grid = grid
+  end
+  
+  attr_reader :grid
+  
+  def subcases
+    rtn = []
+    0.upto(14) do |i|
+      i.upto(15) do |j|
+        subgrid = grid.dup
+        subgrid[i] = grid[j]
+        subgrid[j] = grid[i]
+        rtn << subgrid
+      end
+    end
+  end
+      
   
 end
--- solver.rb
+++ solver.rb
@@ -10,41 +10,12 @@ GRID = [
   "G", "N", "A", "A",
 ]
 
-COMBOS = [
-  [ 0, 1, 2, 3], # row 1
-  [ 4, 5, 6, 7], # row 2
-  [ 8, 9,10,11], # row 3
-  [12,13,14,15], # row 4
-  
-  [ 0, 4, 8,12], # col 1
-  [ 1, 5, 9,13], # col 2
-  [ 2, 6,10,14], # col 3
-  [ 3, 7,11,15], # col 4
-]
-
 WORDS = [
   "JAR",
   "BEAN",
   "BARB",
   "BAR",
 ]
-
-def all_combos(grid)
-  COMBOS.inject([]) do |combos, indices|
-    all_4 = grid.values_at(*indices)
-    combos << all_4
-    combos << all_4[0,3]
-    combos << all_4[1,3]
-    
-    combos
-  end
-end
-
-def all_words(grid)
-  all_combos(grid).select do |combo|
-    is_word?(combo)
-  end
-end
 
 def is_word?(combo)
   WORDS.include?(combo.join(""))
@@ -55,8 +26,11 @@ end
 # end
 
 require 'scorer'
+require 'case'
 
-all_words(GRID).each do |combo|
+base_case = Case.new(GRID)
+
+base_case.all_words.each do |combo|
   puts combo.inspect
   puts "score #{ Scorer.score(combo) }"
 end
