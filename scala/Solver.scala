4f7041622fcfad553e69d0c770df04a4ca30d485
Matthew Rudy Jacobs <MatthewRudyJacobs@gmail.com>
2010-08-10 23:16:20 +0100

    re-indent and work towards better scoring

--- scala/Solver.scala
+++ scala/Solver.scala
@@ -27,43 +27,48 @@ object Scorer {
 		"W" -> 4,
 		"X" -> 8,
 		"Y" -> 4,
-		"Z" -> 10
-	)
-	
+		"Z" -> 10)
+
 	val WORDS = io.Source.fromFile("3letters.dictionary").getLines.toList :::
-	            io.Source.fromFile("4letters.dictionary").getLines.toList
-	
+	io.Source.fromFile("4letters.dictionary").getLines.toList
+
 }
 
 println(Scorer.WORDS)
 println("score for Y is " + Scorer.LETTER_SCORES("Y"))
 
 class Word(val letters : List[String]) {
-	
+
+    // JAR :
+    // J = 8
+    // A = 1
+    // R = 1
+    // scrabble score = 8 + 1 + 1 = 10
+    // actual score = 3 * 10 = 30
 	def score() : Int = {
 		if(isWord) {
-		  val scrabbleScore = letters.foldLeft(0)((sum, letter) => sum + Scorer.LETTER_SCORES(letter))
-		  scrabbleScore * letters.length
+			val scrabbleScore = letters.foldLeft(0)((sum, letter) => sum + Scorer.LETTER_SCORES(letter))
+			scrabbleScore * letters.length
 		}
 		else 0
-    }
+	}
 
-    def isWord() = {
-	  Scorer.WORDS contains this.toString
-    }
+	def isWord() = {
+		Scorer.WORDS contains this.toString
+	}
 
-    override def toString() = letters.mkString("")
+	override def toString() = letters.mkString("")
 
 }
 
 object Word {
-	
+
 	def apply(letters : String*) = {
 		new Word(letters.toList)
 	}
-	
+
 }
-		
+
 
 val jar = Word("J", "A", "R")
 println("word is " + jar.toString())
@@ -78,43 +83,36 @@ println("gar is a word? : " + gar.isWord())
 println("score for gars is " + gar.score())
 
 class Grid(val rowList : List[List[String]]) {
-	
+
 	val colList = rowList.transpose
-	
+
 	def score() : Int = {
 		var sum = 0
-		rowList.foreach {
-			row => row
+		val addWordScores = {
+			line => val letters;
 			
-			val word4 = new Word(row)
-			sum = sum + word4.score()
+			val word4  = new Word(letters)
+			val word3a = new Word(letters.slice(0,3))
+			val word3b = new Word(letters.slice(1,4))
 			
-			val word4rev = new Word(row.reverse)
-			sum = sum + word4rev.score()
+			sum = sum + word4.score() + word3a.score() + word3b.score()
 		}
-		colList.foreach {
-			col => col
-			
-			val word4 = new Word(col)
-			sum = sum + word4.score()
-			
-			val word4rev = new Word(col.reverse)
-			sum = sum + word4rev.score()
-		}
-		return sum
+		rowList.foreach { addWordScores }
+		colList.foreach { addWordScores }
+		sum
 	}
 }
 
 object Grid {
-	
+
 	def apply(letters:String*) = {
 		val list = letters.toList
-		
+
 		val row1 = list.slice( 0, 4)
 		val row2 = list.slice( 4, 8)
 		val row3 = list.slice( 8,12)
 		val row4 = list.slice(12,16)
-		
+
 		new Grid( List(row1, row2, row3, row4) )
 	}
 }
@@ -124,8 +122,8 @@ val grid = Grid(
 	"F", "A", "J", "E",
 	"H", "A", "A", "A",
 	"G", "N", "A", "A"
-)
+	)
 
-println("grid row 1 is " + grid.rowList(0))
-println("grid col 1 is " + grid.colList(0))
-println("grid score is " + grid.score)
+	println("grid row 1 is " + grid.rowList(0))
+	println("grid col 1 is " + grid.colList(0))
+	println("grid score is " + grid.score)
