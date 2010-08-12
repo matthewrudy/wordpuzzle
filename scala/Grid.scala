ae82c4ef64feb266955dc1e753a2b302ebed0585
Matthew Rudy Jacobs <MatthewRudyJacobs@gmail.com>
2010-08-12 15:53:53 +0100

    split the files up

---
+++ scala/Grid.scala
@@ -0,0 +1,125 @@
+class Grid(val letters : List[String], val wordBucket : WordBucket) {
+
+  def calculateScore() = {
+    validWords.foldLeft(0) { (sum, p) =>
+      val position = p._1
+      val word     = p._2
+      
+      sum + Word.score(word)
+    }
+  }
+  
+  def validWords() = {
+    possibleWords.filter { p =>
+      val position = p._1
+      val word     = p._2
+      
+      Word.isWord(word) && wordBucket.valid(position, word)
+    }
+  }
+  
+  def possibleWords = {
+    Grid.POSITIONS.map { p =>
+      val position = p._1
+      val indices  = p._2
+      val word     = indices.map(index => letters(index)).mkString("")
+      
+      position -> word
+    }
+  }
+
+  val score = this.calculateScore()
+  
+  def nextMove(swap1:Int, swap2:Int) = {
+    val letter1 = letters(swap1)
+    val letter2 = letters(swap2)
+    
+    val newLetters = letters.patch(swap1, List(letter2), 1)
+                            .patch(swap2, List(letter1), 1)
+
+    new Grid(newLetters, wordBucket.merge(validWords))
+  }
+}
+
+object Grid {
+  
+  val ROW_POSITIONS = Map(
+    "row1-4"  -> List( 0, 1, 2, 3),
+    "row1-3a" -> List( 0, 1, 2   ),
+    "row1-3b" -> List(    1, 2, 3),
+
+    "row2-4"  -> List( 4, 5, 6, 7),
+    "row2-3a" -> List( 4, 5, 6   ),
+    "row2-3b" -> List(    5, 6, 7),
+
+    "row3-4"  -> List( 8, 9,10,11),
+    "row3-3a" -> List( 8, 9,10   ),
+    "row3-3b" -> List(    9,10,11),
+
+    "row4-4"  -> List(12,13,14,15),
+    "row4-3a" -> List(12,13,14   ),
+    "row4-3b" -> List(   13,14,15)
+  )
+
+  val COL_POSITIONS = Map(
+    "col1-4"  -> List( 0, 4, 8,12),
+    "col1-3a" -> List( 0, 4, 8   ),
+    "col1-3b" -> List(    4, 8,12),
+
+    "col2-4"  -> List( 1, 5, 9,13),
+    "col2-3a" -> List( 1, 5, 9   ),
+    "col2-3b" -> List(    5, 9,13),
+
+    "col3-4"  -> List( 2, 6,10,14),
+    "col3-3a" -> List( 2, 6,10   ),
+    "col3-3b" -> List(    6,10,14),
+
+    "col4-4"  -> List( 3, 7,11,15),
+    "col4-3a" -> List( 3, 7,11   ),
+    "col4-3b" -> List(    7,11,15)
+  )
+  
+  val POSITIONS = ROW_POSITIONS ++ COL_POSITIONS
+
+  def apply(letters:String*) = {
+    new Grid(letters.toList, WordBucket())
+  }
+}
+
+object WordBucket {
+  
+  val EMPTY_BUCKET = Grid.POSITIONS.map { p =>
+    val position = p._1
+    val indices  = p._2
+    
+    position -> Set[String]()
+  }
+   
+  def apply() = {
+    new WordBucket(EMPTY_BUCKET)
+  }
+}
+
+class WordBucket(val bucket:Map[String, Set[String]]) {
+  
+  // have we hit this before in this position
+  def valid(position:String, word:String) = {
+    !(bucket(position) contains word)
+  }
+  
+  // at the next move
+  def merge(newWords:Map[String, String]) = {
+    val newBucket = bucket.map { p => 
+      val position = p._1
+      var words    = p._2
+      
+      if(newWords.isDefinedAt(position)) {
+        words = words + newWords(position)
+      }
+      
+      position -> words
+    }
+    new WordBucket(newBucket)
+  }
+      
+}
---
+++ scala/Word.scala
@@ -0,0 +1,54 @@
+object Word {
+  
+  def scrabbleScore(word:String) = {
+    word.foldLeft(0) {
+      (sum, letter) => sum + letterScore(letter)
+    }
+  }
+  
+  /*scrabble scores*/
+  val LETTER_SCORES = Map(
+    "A" -> 1,
+    "B" -> 3,
+    "C" -> 3,
+    "D" -> 2,
+    "E" -> 1,
+    "F" -> 4,
+    "G" -> 2,
+    "H" -> 4,
+    "I" -> 1,
+    "J" -> 8,
+    "K" -> 5,
+    "L" -> 1,
+    "M" -> 3,
+    "N" -> 1,
+    "O" -> 1,
+    "P" -> 3,
+    "Q" -> 10,
+    "R" -> 1,
+    "S" -> 1,
+    "T" -> 1,
+    "U" -> 1,
+    "V" -> 4,
+    "W" -> 4,
+    "X" -> 8,
+    "Y" -> 4,
+    "Z" -> 10
+  )
+  
+  def letterScore(character:Char) = LETTER_SCORES(character.toString)
+  
+  // JAR :
+  // J = 8
+  // A = 1
+  // R = 1
+  // scrabble score = 8 + 1 + 1 = 10
+  // actual score = 3 * 10 = 30
+  def score(word:String) = scrabbleScore(word) * word.length
+  
+  val WORDS = io.Source.fromFile("dict/3letters").getLines.toList :::
+              io.Source.fromFile("dict/4letters").getLines.toList
+  
+  def isWord(word:String) = WORDS contains word
+
+}
