260beb29520fb826c050251edba1f4298388d799
Matthew Rudy Jacobs <MatthewRudyJacobs@gmail.com>
2010-08-17 02:33:27 +0100

    add one of rich's examples

--- Solver.scala
+++ Solver.scala
@@ -2,13 +2,20 @@ object Solver {
 
   def main(args:Array[String]) {
 
-    val grid1 = Grid(
-      "B", "A", "R", "B",
-      "F", "A", "J", "E",
-      "H", "A", "A", "A",
-      "G", "N", "R", "A"
-    )
+    val ex1 = Grid(
+      "E", "I", "P", "X",
+      "U", "R", "O", "A",
+      "A", "W", "E", "H",
+      "E", "A", "I", "E"
+      )
+      
+    ex1.print()
+    println("example 1 score : "+ex1.score)
+    println("words: " +ex1.validWords)
     
-    println(grid1.bestMove.score)
+    val ex2 = ex1.nextMove(3,11)
+    ex2.print()
+    println("example 2 score : "+ex2.score)
+    println("words: " +ex2.validWords)
   }
 }
