7c3754877ccf1b9efec8d2c0e33a3539bd4f44d1
Matthew Rudy Jacobs <MatthewRudyJacobs@gmail.com>
2010-08-17 17:05:49 +0100

    tidy up

--- Solver.scala
+++ Solver.scala
@@ -11,44 +11,32 @@ object Solver {
       
     ex1.print()
     
-    /*val next2 = ex1.best2Moves
-        val move1 = next2(0)
-        move1.print
-        val move2 = next2(1)
-        move2.print
-        
-        val last2 = move2.best2Moves
-        val move3 = last2(0)
-        move3.print
-        val move4 = last2(1)
-        move4.print
-        
-        val move5 = move2.bestMove
-        move5.print*/
-    
     val next3 = ex1.best3Moves
+    
+    println("move 1:")
     val move1 = next3(0)
     move1.print
+    
+    println("move 2:")
     val move2 = next3(1)
     move2.print
+    
+    println("move 3:")
     val move3 = next3(2)
     move3.print
     
     val last2 = move3.best2Moves
+    
+    println("move 4:")
     val move4 = last2(0)
     move4.print
     
+    println("move 5:")
     val move5 = last2(1)
     move5.print
-   /* val move1 = ex1.bestMove
-       move1.print
-       val move2 = move1.bestMove
-       move2.print
-       val move3 = move2.bestMove
-       move3.print
-       val move4 = move3.bestMove
-       move4.print
-       val move5 = move4.bestMove
-       move5.print*/
+    
+    println()
+    println("total: " move1.score + move2.score + move3.score + move4.score + move5.score)
   }
+   
 }
